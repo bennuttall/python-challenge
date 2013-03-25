32a9d306b0dbde1a452cc53b4f5c35b810c8a402
Ben Nuttall <ben@bennuttall.com>
2013-03-25 01:07:21 +0000

    Refactored and made pep8 compliant

--- level_04/level_4.py
+++ level_04/level_4.py
@@ -1,22 +1,23 @@
-import urllib
+from urllib2 import urlopen
 
 url = 'http://www.pythonchallenge.com/pc/def/linkedlist.php?nothing='
 n = '12345'
 
-while 1:
+while True:
     urln = url + n
-    nxt = urllib.urlopen(urln).read()
+    nxt = urlopen(urln).read()
     #print nxt
     n = ''
     for i in range(len(nxt)):
         try:
-            if nxt[i:i+24] == 'and the next nothing is ':
-                n = nxt[i+24:]
+            if nxt[i: i + 24] == 'and the next nothing is ':
+                n = nxt[i + 24:]
         except:
             pass
     if n == '':
         if nxt == 'Yes. Divide by two and keep going.':
-            n = str(92118/2)
+            n = str(92118 / 2)
         else:
-            #print nxt
             break
+
+print nxt
